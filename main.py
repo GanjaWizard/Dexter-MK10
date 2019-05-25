f9b861a53097be146b8fd2566a160a68448a3840
Owen Cameron Easter <oceaster@gmail.com>
2019-05-25 01:32:08 +0100

    0.0008

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # MARK 10 / MKX
 # AUTHOR : EASTER LTD
 
+import platform
+import subprocess
 import urllib.request as urlreq
 from time import time
-from datetime import date
 
 
 class MarkXCore:
@@ -12,15 +13,24 @@ class MarkXCore:
     def __init__(self):
         self.build = [
             time(),
-            0.0007,
+            0.0008,
             'a',
+            platform.system()
         ]
         print('DEXTER VIRTUAL INTELLIGENCE MARK 10:', self.build)
 
     def run(self):
-        print('\n\nHello World.')
-        print('\nThe time is currently ', time())
-
+        # OPERATING SYSTEM CHECKER
+        if self.build[3] == 'Windows':
+            pass
+        elif self.build[3] == 'Linux':
+            subprocess.run(['sudo', 'apt-get', 'install',
+                            'bluez', 'pulseaudio-module-bluetooth',
+                            'python-gobject', 'python-gobject-2',
+                            'bluez-tools'])
+        else:
+            print(self.build[3], 'NOT SUPPORTED')
+        # ----
 
 if __name__ == '__main__':
     # SOFT INITIALIZATION
@@ -28,7 +38,7 @@ if __name__ == '__main__':
     urlreq.urlcleanup()
     print('>> SYSTEM UPDATE')
     # CHECK UPDATE REPOSITORY
-    http = urlreq.urlopen("https://github.com/Oweneaster/mkx")
+    http = urlreq.urlopen("https://github.com/Oweneaster/Dexter-MK10")
     html = http.read()
 
     def disc(html, label):
@@ -38,14 +48,20 @@ if __name__ == '__main__':
 
     vers = float(disc(html=html, label='/VERSION'))
     print(' LATEST VERSION:', vers)
+
     # SYSTEM VERSION CHECKING
     if vers > Core.build[1]:
         print(' SYSTEM IS OUT-OF-DATE\n  Updating...')
-        data = urlreq.urlretrieve("https://raw.github.com/Oweneaster/mkx/master/main.py", "main.py")
-        print('  Success!')
+        data = urlreq.urlretrieve("https://raw.github.com/Oweneaster/Dexter-MK10/master/main.py", "main.py")
+        print('  Success!\n')
+        if Core.build[3] == 'Windows':
+            subprocess.run(['python', 'main.py'])
+        else:
+            subprocess.run(['python3', 'main.py'])
+
     elif vers == Core.build[1]:
-        print(' SYSTEM IS UP-TO-DATE')
+        print(' SYSTEM IS UP-TO-DATE\n')
         Core.run()
     else:
-        print(' REPOSITORY NOT SUPPORTED')
-        exit()
+        print(' REPOSITORY NOT SUPPORTED\n')
+        Core.run()
